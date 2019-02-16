70f1fa29388026f42fa585c1d571650a5af5985c
Lincxx <bossco28@gmail.com>
2019-02-16 18:00:20 -0500

    Added in a state less widget, to generate the random sentences

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,60 @@ import 'package:random_words/random_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final noun = WordNoun.random();
-  final adjective = WordAdjective.random();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Word Game',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Word Game'),
-        ),
-        body: Center(
-          child: Text(
-              'The programmer wrote a ${adjective.asCapitalized} app in Flutter and showed it off to his ${noun.asCapitalized}'),
-        ),
+    return MaterialApp(title: 'Word Game', home: RandomSentences());
+  }
+}
+
+class RandomSentences extends StatefulWidget {
+  @override
+  _RandomSentencesState createState() => _RandomSentencesState();
+}
+
+class _RandomSentencesState extends State<RandomSentences> {
+  final _sentences = <String>[];
+  final _biggerFont = TextStyle(fontSize: 14.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Word Game'),
       ),
+      body: _buildSentences(),
+    );
+  }
+
+  String _getSentence() {
+    final noun = WordNoun.random();
+    final adjective = WordAdjective.random();
+    return 'The programmer wrote a ${adjective.asCapitalized} app in Flutter and showed it off to his ${noun.asCapitalized}';
+  }
+
+  Widget _buildRow(String sentence) {
+    return ListTile(
+      title: Text(
+        sentence,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSentences() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+        final index = i ~/ 2;
+        if (index >= _sentences.length) {
+          for (int x = 0; x < 10; x++) {
+            _sentences.add(_getSentence());
+          }
+        }
+        return _buildRow(_sentences[index]);
+      },
     );
   }
 }
