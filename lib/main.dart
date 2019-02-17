6f88fa94c082296236f3bd5bb4ee302e445622b3
Lincxx <bossco28@gmail.com>
2019-02-17 12:20:00 -0500

    Added in a saved page and a favorite button

--- lib/main.dart
+++ lib/main.dart
@@ -19,29 +19,83 @@ class RandomSentences extends StatefulWidget {
 class _RandomSentencesState extends State<RandomSentences> {
   final _sentences = <String>[];
   final _biggerFont = TextStyle(fontSize: 14.0);
+  final _funnies = Set<String>(); //sets don't allow for duplicates
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Word Game'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushFunnies,
+          ),
+        ],
       ),
       body: _buildSentences(),
     );
   }
 
+  void _pushFunnies() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          final tiles = _funnies.map(
+            (sentence) {
+              return ListTile(
+                title: Text(
+                  sentence,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Funny Sentences'),
+            ),
+            body: ListView(
+              children: divided,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
   String _getSentence() {
     final noun = WordNoun.random();
+
     final adjective = WordAdjective.random();
     return 'The programmer wrote a ${adjective.asCapitalized} app in Flutter and showed it off to his ${noun.asCapitalized}';
   }
 
   Widget _buildRow(String sentence) {
+    final alreadyFoundFunny = _funnies.contains(sentence);
     return ListTile(
       title: Text(
         sentence,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadyFoundFunny ? Icons.thumb_up : Icons.thumb_down,
+        color: alreadyFoundFunny ? Colors.green : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadyFoundFunny) {
+            _funnies.remove(sentence);
+          } else {
+            _funnies.add(sentence);
+          }
+        });
+      },
     );
   }
 
